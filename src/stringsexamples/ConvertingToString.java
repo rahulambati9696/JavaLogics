3be975d83ae62c50b1e22f333bfe6fb9f92b38b2
rahulambati9696 <ambati.rahulkumar96@gmail.com>
2022-03-24 18:16:09 +0530

    Modified .....

--- src/stringsexamples/ConvertingToString.java
+++ src/stringsexamples/ConvertingToString.java
@@ -18,15 +18,19 @@ public class ConvertingToString
 		int v1=20; 
 		String s1=String.valueOf(v1);
 		System.out.println(s1+'_'+17);       //concatenating string with 10 
+		
 		float v2=2.5f;
 		String s2=String.valueOf(v2);
 		System.out.println(s2+'_'+17); 
+		
 		char v3='a';
 		String s3=String.valueOf(v3); 
 		System.out.println(s3+'_'+17); 
+		
 		long v4=984384748;
 		String s4=String.valueOf(v4); 
 		System.out.println(s4+'_'+17); 
+		
 		double v5=373.73737388387;
 		String s5=String.valueOf(v5);
 		System.out.println(s5+'_'+17); 
