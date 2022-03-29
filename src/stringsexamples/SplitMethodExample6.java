b6c1e402b1e170ea3ca0a80027163cc19d677578
rahulambati9696 <ambati.rahulkumar96@gmail.com>
2022-03-29 12:54:48 +0530

    Commit String Changes.....

--- src/stringsexamples/SplitMethodExample6.java
+++ src/stringsexamples/SplitMethodExample6.java
@@ -4,7 +4,7 @@ public class SplitMethodExample6
 {
 	public static void main(String args[])
 	{
-		String str = "word1, word2 word3@word4?word5.word6";
+		String str = "word1, word2 word3@word4?word5.word6,word7";
 		String[] arrOfStr = str.split("[, ?.@]+");
 
 		for (String a : arrOfStr)
